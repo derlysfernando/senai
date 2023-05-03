b08dda7f422974441aeeba5c60d125feb82c0671
Derlys <derlys.fern@gmail.com>
2023-05-03 16:19:35 -0300

    codes

--- Assets/GameAssets/Script/ItemCollector.cs
+++ Assets/GameAssets/Script/ItemCollector.cs
@@ -5,20 +5,5 @@ using TMPro;
 
 public class ItemCollector : MonoBehaviour
 {
-    private int cherries = 0;
-
-    [SerializeField] private TMP_Text cherriesText;
-
-    // [SerializeField] private AudioSource collectionSoundEffect;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("Cherry"))
-        {
-            // collectionSoundEffect.Play();
-            Destroy(collision.gameObject);
-            cherries++;
-            cherriesText.text = "Cherries: " + cherries.ToString();
-        }
-    }
+   
 }
